c14d19d1c348507f1a3e7a5e5e85878dacae1b28
Kakaluoto <1052176873@qq.com>
2022-01-18 12:51:45 +0800

    2022/1/18

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ import socket
 import threading
 import pyaudio
 
+
 class Client:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         while 1:
             try:
                 self.target_ip = input('Enter IP address of server --> ')
@@ -19,16 +20,18 @@ class Client:
             except:
                 print("Couldn't connect to server")
 
-        chunk_size = 1024 # 512
+        chunk_size = 1024  # 512
         audio_format = pyaudio.paInt16
         channels = 1
         rate = 20000
 
         # initialise microphone recording
         self.p = pyaudio.PyAudio()
-        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True, frames_per_buffer=chunk_size)
-        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk_size)
-        
+        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True,
+                                          frames_per_buffer=chunk_size)
+        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
+                                            frames_per_buffer=chunk_size)
+
         print("Connected to Server")
 
         # start threads
@@ -43,7 +46,6 @@ class Client:
             except:
                 pass
 
-
     def send_data_to_server(self):
         while True:
             try:
@@ -51,5 +53,6 @@ class Client:
                 self.s.sendall(data)
             except:
                 pass
+
 
 client = Client()
--- server.py
+++ server.py
@@ -3,36 +3,37 @@
 import socket
 import threading
 
+
 class Server:
     def __init__(self):
-            self.ip = socket.gethostbyname(socket.gethostname())
-            while 1:
-                try:
-                    self.port = int(input('Enter port number to run on --> '))
+        self.ip = socket.gethostbyname(socket.gethostname())
+        while 1:
+            try:
+                self.port = int(input('Enter port number to run on --> '))
 
-                    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self.s.bind((self.ip, self.port))
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s.bind((self.ip, self.port))
 
-                    break
-                except:
-                    print("Couldn't bind to that port")
+                break
+            except:
+                print("Couldn't bind to that port")
 
-            self.connections = []
-            self.accept_connections()
+        self.connections = []
+        self.accept_connections()
 
     def accept_connections(self):
         self.s.listen(100)
 
-        print('Running on IP: '+self.ip)
-        print('Running on port: '+str(self.port))
-        
+        print('Running on IP: ' + self.ip)
+        print('Running on port: ' + str(self.port))
+
         while True:
             c, addr = self.s.accept()
 
             self.connections.append(c)
 
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
-        
+            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
+
     def broadcast(self, sock, data):
         for client in self.connections:
             if client != self.s and client != sock:
@@ -41,13 +42,14 @@ class Server:
                 except:
                     pass
 
-    def handle_client(self,c,addr):
+    def handle_client(self, c, addr):
         while 1:
             try:
                 data = c.recv(1024)
                 self.broadcast(c, data)
-            
+
             except socket.error:
                 c.close()
+
 
 server = Server()
